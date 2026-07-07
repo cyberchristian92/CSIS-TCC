e7828db4f62a41501583dc130d88618249ebc1db
cyberchristian92 <cyberchristian92@gmail.com>
2026-07-07 07:45:30 -0300

    Fase 8: Tipos de Projetos e Top Bar (Fix syntax)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1916138497" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1900944185" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
