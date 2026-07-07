ad3a05af3d137542af729fb9aa8ea01fb48c21d9
cyberchristian92 <cyberchristian92@gmail.com>
2026-07-07 07:09:44 -0300

    Fase 7: Missoes, Kanban e Fila de Revisao

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3464164725" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2581013114" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
